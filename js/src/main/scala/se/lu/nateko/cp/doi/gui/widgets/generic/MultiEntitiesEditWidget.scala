c8919f500b6570ed75cd78d152f0f8f022f9d162
Oleg Mirzov <oleg.mirzov@nateko.lu.se>
2018-02-15 17:38:55 +0100

    MultiEntitiesEditWidget is now collapsible

--- js/src/main/scala/se/lu/nateko/cp/doi/gui/widgets/generic/MultiEntitiesEditWidget.scala
+++ js/src/main/scala/se/lu/nateko/cp/doi/gui/widgets/generic/MultiEntitiesEditWidget.scala
@@ -1,6 +1,6 @@
 package se.lu.nateko.cp.doi.gui.widgets.generic
 
-import scalatags.JsDom.all._
+import scalatags.JsDom.all.{title => htmlTitle, _}
 import scala.collection.mutable.Buffer
 import org.scalajs.dom.Event
 import se.lu.nateko.cp.doi.gui.views.Bootstrap
@@ -11,6 +11,7 @@ abstract class MultiEntitiesEditWidget[E, W <: EntityWidget[E]](initValues: Seq[
 	protected def makeWidget(value: E, updateCb: E => Unit): W
 	protected def defaultValue: E
 	protected val minAmount: Int
+	private var isCollapsed = false
 
 	private val widgets = Buffer.empty[RemovableEntityWidget[E]]
 
@@ -29,31 +30,62 @@ abstract class MultiEntitiesEditWidget[E, W <: EntityWidget[E]](initValues: Seq[
 			widgets -= widget
 			widgetsParent.removeChild(widget.element)
 			setRemovability()
+			setCollapsedness()
 			notifyUpstream()
 		})
 		widgetsParent.appendChild(newWidget.element)
 		widgets += newWidget
 	}
-	initValues.foreach(produceWidget)
-	setRemovability()
 
 	private val addWidget: Event => Unit = (_: Event) => {
 		produceWidget(defaultValue)
 		setRemovability()
+		setCollapsedness()
 		notifyUpstream()
+	}
+
+	private val collapseWidget: Event => Unit = (_: Event) => {
+		isCollapsed = !isCollapsed
+		setCollapsedness()
+	}
+
+	private val collapseIcon = span().render
+	private val collapseButton = button(
+			tpe := "button",
+			cls := "btn btn-default",
+			onclick := collapseWidget,
+			marginBottom := 5
+		)(collapseIcon).render
+
+	private val addWidgetButton = button(
+			tpe := "button", cls := "btn btn-success",
+			htmlTitle := "Add another item to the list",
+			onclick := addWidget, marginBottom := 5
+		)(
+			span(cls := "glyphicon glyphicon-plus")
+		).render
+
+	private def setCollapsedness(): Unit = {
+		val canCollapse: Boolean = widgetsParent.childNodes.length > 0
+		collapseButton.style.display = if(canCollapse) "inline-block" else "none"
+		collapseIcon.className = "glyphicon glyphicon-collapse-" + (if(isCollapsed) "down" else "up")
+		collapseButton.title = if(isCollapsed) "Expand this list back down" else "Collapse this list up"
+		widgetsParent.style.display = if(isCollapsed) "none" else "block"
+		addWidgetButton.style.display = if(isCollapsed) "none" else "inline-block"
 	}
 
 	val element = Bootstrap.basicPanel(
 		div(cls := "row")(
 			div(cls := "col-md-1")(
-				span(strong(title)),
-				raw("&nbsp;"),
-				button(tpe := "button", cls := "btn btn-success", onclick := addWidget, marginBottom := 5)(
-					span(cls := "glyphicon glyphicon-plus")
-				)
+				div(strong(title)),
+				div(addWidgetButton, collapseButton)
 			),
 			widgetsParent
 		)
 	)
+
+	initValues.foreach(produceWidget)
+	setRemovability()
+	setCollapsedness()
 
 }
