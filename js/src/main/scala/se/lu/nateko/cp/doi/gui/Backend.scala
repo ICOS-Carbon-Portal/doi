4143b8e3f9cbab56b2423e924162bd92057536a8
Oleg Mirzov <oleg.mirzov@gmail.com>
2021-11-01 17:20:32 +0100

    Code polishing in Backend.scala; scalajs to 1.7.1

--- js/src/main/scala/se/lu/nateko/cp/doi/gui/Backend.scala
+++ js/src/main/scala/se/lu/nateko/cp/doi/gui/Backend.scala
@@ -16,13 +16,9 @@ case class DoiWithTitle(doi: Doi, title: String)
 
 object Backend {
 
-	private def parseTo[T : Reads](jsonString: String): T = {
-		Json.parse(jsonString.toString()).as[T]
-	}
-
 	def getPrefixInfo: Future[String] = dom
 		.fetch("/api/doiprefix")
-		.flatMap(checkResponseOk(_, "fetch DOI prefix"))
+		.flatMap(checkResponseOk("fetch DOI prefix"))
 		.flatMap(_.text())
 
 	def updateMeta(meta: DoiMeta): Future[Unit] = dom
@@ -34,16 +30,17 @@ object Backend {
 				headers = new dom.Headers(js.Dictionary(("content-type", "application/json")))
 			}
 		)
-		.flatMap(checkResponseOk(_, "update DOI metadata").map(_ => ()))
+		.flatMap(checkResponseOk("update DOI metadata"))
+		.map(_ => ())
 
 	def getFreshDoiList(query: Option[String], page: Option[Int]): Future[FreshDoiList] = {
 		//val startTime = System.currentTimeMillis()
 		dom
 			.fetch(s"/api/list/?query=${query.getOrElse("")}&page=${page.getOrElse(1)}")
 			//.andThen{case _ => println(s"Got list response in ${System.currentTimeMillis() - startTime} ms")}
-			.flatMap(checkResponseOk(_, "fetch DOI list from DataCite REST API"))
+			.flatMap(checkResponseOk("fetch DOI list from DataCite REST API"))
 			.flatMap(_.text())
-			.map(parseTo[DoiListPayload])
+			.map(s => Json.parse(s).as[DoiListPayload])
 			.map{pl =>
 				val dois = pl.data.map(_.attributes)
 				//println(s"Got list response and parsed DOIs in ${System.currentTimeMillis() - startTime} ms")
@@ -51,15 +48,17 @@ object Backend {
 			}
 	}
 
-	def delete(doi: Doi): Future[dom.Response] = dom
+	def delete(doi: Doi): Future[Unit] = dom
 		.fetch(s"/api/$doi/", new dom.RequestInit{method = dom.HttpMethod.DELETE})
-		.flatMap(checkResponseOk(_, "delete DOI"))
+		.flatMap(checkResponseOk("delete DOI"))
+		.map(_ => ())
 
-	def submitForPublication(doi: Doi): Future[dom.Response] = dom
+	def submitForPublication(doi: Doi): Future[Unit] = dom
 		.fetch(s"/api/submit/$doi", new dom.RequestInit{method = dom.HttpMethod.POST})
-		.flatMap(checkResponseOk(_, "submit DOI for publication"))
+		.flatMap(checkResponseOk("submit DOI for publication"))
+		.map(_ => ())
 
-	private def checkResponseOk(resp: dom.Response, hint: String): Future[dom.Response] =
+	private def checkResponseOk(hint: String)(resp: dom.Response): Future[dom.Response] =
 		if(resp.ok) Future.successful(resp)
 		else Future.failed(new Exception(s"Got response ${resp.statusText} when trying to $hint"))
 }
