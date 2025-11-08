803deb5be315dc3d88db0cee68e71cf7bf307d97
Jonathan Thiry <github@jonathanthiry.com>
2025-11-08 19:56:04 +0100

    WIP - doi viewer layout

--- js/src/main/scala/se/lu/nateko/cp/doi/gui/DoiMetaViewer.scala
+++ js/src/main/scala/se/lu/nateko/cp/doi/gui/DoiMetaViewer.scala
@@ -10,7 +10,7 @@ import se.lu.nateko.cp.doi.gui.widgets.TabWidget
 class DoiMetaViewer(meta: DoiMeta, tabsCb: Map[EditorTab, () => Unit], cloneCb: DoiMeta => Unit) {
 
 	private val tabs = new TabWidget(EditorTab.view, tabsCb).element
-	private val titles = meta.titles.fold[Seq[String]](Seq())(_.map(_.title).toSeq).map(h2(cls := "fs-3")(_))
+	
 	private val subtitleProperties =
 		Seq(
 			meta.types.map(t => Seq(t.resourceTypeGeneral, t.resourceType).filter(_.nonEmpty).flatten.mkString(" / ")),
@@ -18,35 +18,87 @@ class DoiMetaViewer(meta: DoiMeta, tabsCb: Map[EditorTab, () => Unit], cloneCb: 
 			meta.publicationYear.map(year => s"published in $year"),
 			meta.publisher.map(publisher => s"by $publisher")
 		).filter(_.nonEmpty).flatten
-	private val subtitle = p(cls := "text-muted")(subtitleProperties.mkString(" "))
-	private val description = meta.descriptions.map(d => p(d.description)).toSeq
-	private val subjects = p(meta.subjects.map(subject => span(cls := "badge rounded-pill bg-secondary me-1")(subject.subject)).toSeq)
-	private val creators = div(listPeople(meta.creators.toSeq, "Creators"))
-	private val contributors = div(listPeople(meta.contributors.toSeq, "Contributors"))
-	private val dates = p(meta.dates.map(d => div(d.toString)).toSeq)
+	
+	private val subtitle = div(cls := "text-muted mb-4")(
+		span(subtitleProperties.mkString(" "))
+	)
+	
+	private val description = if (meta.descriptions.nonEmpty) {
+		Seq(
+			h5(cls := "mt-4 mb-3")("Description"),
+			div(cls := "mb-3")(
+				meta.descriptions.map(d => p(cls := "mb-2")(d.description)).toSeq
+			)
+		)
+	} else Seq()
+	
+	private val subjects = if (meta.subjects.nonEmpty) {
+		Seq(
+			h5(cls := "mt-4 mb-3")("Subjects"),
+			div(cls := "mb-3")(meta.subjects.map(subject => 
+				span(cls := "badge rounded-pill bg-secondary me-2 mb-2")(subject.subject)
+			).toSeq)
+		)
+	} else Seq()
+	
+	private val creators = listPeople(meta.creators.toSeq, "Creators").map { creatorText =>
+		Seq(
+			h5(cls := "mt-4 mb-3")("Creators"),
+			div(cls := "mb-3")(
+				span(creatorText.replaceFirst("Creators: ", ""))
+			)
+		)
+	}.getOrElse(Seq())
+	
+	private val contributors = listPeople(meta.contributors.toSeq, "Contributors").map { contributorText =>
+		Seq(
+			h5(cls := "mt-4 mb-3")("Contributors"),
+			div(cls := "mb-3")(
+				span(contributorText.replaceFirst("Contributors: ", ""))
+			)
+		)
+	}.getOrElse(Seq())
+	
 	private val doiUrl = "https://doi.org/" + meta.doi
-	private val doiLink = p(a(href := doiUrl, target := "_blank")(doiUrl))
 	private val dataciteUrl = "https://commons.datacite.org/doi.org/" + meta.doi
-	private val dataciteLink = p(a(href := dataciteUrl, target := "_blank")(dataciteUrl))
+	private val fabricaUrl = "https://doi.datacite.org/doi.org/" + meta.doi
+	
+	private val links = div(cls := "mb-4")(
+		div(cls := "mb-2")(
+			a(href := doiUrl, target := "_blank", cls := "text-decoration-none")(doiUrl)
+		),
+		div(cls := "mb-2")(
+			a(href := dataciteUrl, target := "_blank", cls := "text-decoration-none")("Commons")
+		),
+		div(
+			a(href := fabricaUrl, target := "_blank", cls := "text-decoration-none")("Fabrica")
+		)
+	)
 
-	private val cloneButton = button(tpe := "button", cls := "btn btn-secondary edit-control")("Clone").render
+	private val cloneButton = button(
+		tpe := "button", 
+		cls := "btn btn-primary edit-control mt-4"
+	)(
+		i(cls := "fa-solid fa-copy me-2"),
+		"Clone DOI Metadata"
+	).render
 	cloneButton.onclick = (_: Event) => cloneCb(meta)
 
 	val element = div(
 		tabs,
 		div(cls := "row")(
 			div(cls := "col-md-8")(
-				subtitle,
-				description,
-				subjects,
-				p(
+				div(cls := "mt-3")(
+					subtitle,
+					links,
+					description,
+					subjects,
 					creators,
-					contributors
-				),
-				dates,
-				doiLink,
-				dataciteLink,
-				cloneButton
+					contributors,
+					div(cls := "d-grid gap-2 mt-4 mb-5")(
+						cloneButton
+					)
+				)
 			)
 		)
 	).render
